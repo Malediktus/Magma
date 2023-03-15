8cc18671226e025324dfb721d2319d03a25b28d9
Nico Grundei <malediktusrgb@gmail.com>
2023-03-15 17:11:09 +0100

    Added google test framework

--- Magma/src/Magma/Core/Application.cpp
+++ Magma/src/Magma/Core/Application.cpp
@@ -1,12 +1,12 @@
 #include <Magma/pch.h>
 #include <Magma/Core/Application.h>
+#include <gtest/gtest.h>
 
 namespace Magma
 {
 	void Application::Run()
 	{
 		EASY_FUNCTION(profiler::colors::Red);
-
 		OnInit();
 		while (mRunning);
 		OnShutdown();
