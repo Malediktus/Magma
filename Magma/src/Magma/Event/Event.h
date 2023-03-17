4665a4a0b3759c84dc18c3baef3b7d5cd09fbc53
Nico Grundei <malediktusrgb@gmail.com>
2023-03-17 22:39:49 +0100

    Added more events for application

--- Magma/src/Magma/Event/Event.h
+++ Magma/src/Magma/Event/Event.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Magma/pch.h>
+#include <Magma/Event/KeyCode.h>
 
 namespace Magma
 {
@@ -8,6 +9,110 @@ namespace Magma
     {
     public:
         virtual ~Event() {}
+    };
+
+    class WindowResizeEvent : public Event
+    {
+    public:
+        WindowResizeEvent(const int &width, const int &height)
+            : m_Width(width), m_Height(height) {}
+
+        const int &GetWidth() { return m_Width; }
+        const int &GetHeight() { return m_Height; }
+
+    private:
+        const int m_Width;
+        const int m_Height;
+    };
+
+    class WindowCloseEvent : public Event
+    {
+    };
+
+    class KeyPressEvent : public Event
+    {
+    public:
+        KeyPressEvent(const KeyCode &keycode)
+            : m_KeyCode(keycode) {}
+
+        const KeyCode &GetKeyCode() { return m_KeyCode; }
+
+    private:
+        const KeyCode m_KeyCode;
+    };
+
+    class KeyReleaseEvent : public Event
+    {
+    public:
+        KeyReleaseEvent(const KeyCode &keycode)
+            : m_KeyCode(keycode) {}
+
+        const KeyCode &GetKeyCode() { return m_KeyCode; }
+
+    private:
+        const KeyCode m_KeyCode;
+    };
+
+    class KeyRepeatEvent : public Event
+    {
+    public:
+        KeyRepeatEvent(const KeyCode &keycode)
+            : m_KeyCode(keycode) {}
+
+        const KeyCode &GetKeyCode() { return m_KeyCode; }
+
+    private:
+        const KeyCode m_KeyCode;
+    };
+
+    class MouseMoveEvent : public Event
+    {
+    public:
+        MouseMoveEvent(const int &X, const int &Y)
+            : m_X(X), m_Y(Y) {}
+
+        const int &GetX() { return m_X; }
+        const int &GetY() { return m_Y; }
+
+    private:
+        const int m_X;
+        const int m_Y;
+    };
+
+    class MousePressEvent : public Event
+    {
+    public:
+        MousePressEvent(const KeyCode &keycode)
+            : m_KeyCode(keycode) {}
+
+        const KeyCode &GetKeyCode() { return m_KeyCode; }
+
+    private:
+        const KeyCode m_KeyCode;
+    };
+
+    class MouseReleaseEvent : public Event
+    {
+    public:
+        MouseReleaseEvent(const KeyCode &keycode)
+            : m_KeyCode(keycode) {}
+
+        const KeyCode &GetKeyCode() { return m_KeyCode; }
+
+    private:
+        const KeyCode m_KeyCode;
+    };
+
+    class MouseScrollEvent : public Event
+    {
+    public:
+        MouseScrollEvent(const int &value)
+            : m_Value(value) {}
+
+        const int &GetValue() { return m_Value; }
+
+    private:
+        const int m_Value;
     };
 
     using EventMap = std::multimap<const std::type_info *, const std::function<void(const Event &)>>;
