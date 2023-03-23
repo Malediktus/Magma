d451c7f12e333a11525a379c78a7ea15d09989eb
Nico Grundei <malediktusrgb@gmail.com>
2023-03-23 17:20:21 +0100

    Switched opengl renderer to version 3.3 and changed some stuff in cmake to make it run on windows

--- Magma/src/Magma/Core/Entry.h
+++ Magma/src/Magma/Core/Entry.h
@@ -1,24 +1,13 @@
 #pragma once
 
 #include <Magma/Core/Application.h>
-#ifdef _DEBUG
-#include <easy/profiler.h>
-#endif
 
 extern Magma::Application* Magma::CreateApplication();
 
 int main(void)
 {
-    #ifdef _DEBUG
-    EASY_PROFILER_ENABLE;
-    #endif
-
     Magma::Application* app = Magma::CreateApplication();
     app->Run();
-
-    #ifdef _DEBUG
-    profiler::dumpBlocksToFile("Magma.prof");
-    #endif
 
     return 0;
 }
