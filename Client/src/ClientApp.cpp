4950527e84df7805064c9cf6fe965283e428b586
Nico Grundei <malediktusrgb@gmail.com>
2023-03-26 00:55:54 +0100

    Added imgui demo window and clear color

--- Client/src/ClientApp.cpp
+++ Client/src/ClientApp.cpp
@@ -8,6 +8,7 @@ public:
     
     void OnInit() override
     {
+        m_ShowDemoWindow = true;
     }
     
     void OnShutdown() override
@@ -20,10 +21,15 @@ public:
     
     void OnDebugUIRender() override
     {
+        if (m_ShowDemoWindow)
+            ImGui::ShowDemoWindow(&m_ShowDemoWindow);
         ImGui::Begin("Test");
         ImGui::Text("ImGui Test Window");
         ImGui::End();
     }
+    
+private:
+    bool m_ShowDemoWindow;
 };
 
 std::shared_ptr<Magma::Application> Magma::CreateApplication()
--- Magma/src/Magma/Platform/OpenGL/OpenGLContext.cpp
+++ Magma/src/Magma/Platform/OpenGL/OpenGLContext.cpp
@@ -20,4 +20,6 @@ void OpenGLContext::Init()
 void OpenGLContext::SwapBuffers()
 {
     glfwSwapBuffers(m_WindowHandle);
+    glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
